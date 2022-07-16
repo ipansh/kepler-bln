57adbfe6a7cdf640450ba306fbed40f491579888
Ilya <ilya.panshenskov@italki.com>
2022-07-16 11:29:12 +0200

    update telegram

--- telegram_notifier.py
+++ telegram_notifier.py
@@ -3,5 +3,5 @@ import requests
 def send_message(text, telegram_token):
 ### permissions for telegram bot token
     bot_url = "https://api.telegram.org/bot{}/".format(telegram_token)
-    url = bot_url + "sendMessage?text={}&chat_id={}".format(text, 33651759)
+    url = bot_url + "sendMessage?text={}&chat_id={}".format(text, 5403720472)
     return requests.get(url)
