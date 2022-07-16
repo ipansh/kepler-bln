c8405d9141b9dca5926df836e054a1b2b496c36f
Ilya <ilya.panshenskov@italki.com>
2022-07-16 10:34:02 +0200

    adding telegram notifier

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 import scraper
+import telegram_notifier
+
+telegram_token = os.environ.get("TELEGRAM_TOKEN")
 
 if __name__  == '__main__': 
     listings_list = scraper.get_wg_gesucht_listings()
     final_df = scraper.scrape_wg_gesucht_data(listings_list)
     filtered_df = scraper.clean_and_filter_gesucht_data(final_df)
+    telegram_notifier.send_message('Job done!',telegram_token)
     print('Done!')
