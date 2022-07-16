302f6f593819c152a0ac74905e721290a6dc0dd0
Ilya <ilya.panshenskov@italki.com>
2022-07-16 12:56:09 +0200

    added gspread functionality

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 import scraper
 import telegram_notifier
+import gspread_wrapper
 
 telegram_token = os.environ.get("TELEGRAM_TOKEN")
+google_service_account = dict(os.environ.get("GOOGLE_SERVICE_ACCOUNT"))
 
 if __name__  == '__main__': 
+    master_df = gspread_wrapper.get_master_spreadsheet()
+    print('Obtained master spreadsheet!')
     listings_list = scraper.get_wg_gesucht_listings()
     final_df = scraper.scrape_wg_gesucht_data(listings_list)
     filtered_df = scraper.clean_and_filter_gesucht_data(final_df)
