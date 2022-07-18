f2127411e7566752c18285cdf1e6d141ac04db27
Ilya <ilya.panshenskov@italki.com>
2022-07-18 19:17:19 +0200

    redeploy

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ import os
 
 telegram_token = os.environ.get("TELEGRAM_TOKEN")
 db_access_key = os.environ.get("POSTGRE")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+selenium_driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 if __name__  == '__main__': 
     cnx = create_engine(db_access_key)
